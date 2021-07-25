5ca1bcfd06cceb11047b3241cb20fe3b3b3ccd10
dvargas42 <dvargas.eng@gmail.com>
2021-07-25 17:22:23 -0300

    feature: import useAuth

--- src/pages/platform/index.tsx
+++ src/pages/platform/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import { api_donations} from '../../services/api'
 import Cookies from 'js-cookie';
 import OngCard from '../../components/Card';
+
+import { useAuth } from '../../context/AuthContext'
 import Filters from '../../components/Filters';
 import CardsContainer from '../../components/CardsContainer';
 
@@ -10,6 +12,7 @@ import styles from './styles.module.scss'
 
 
 export default function Platform(){
+  const { getInstitutions } = useAuth()
 
   async function getPedidos() {
 
